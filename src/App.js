7ca0cfee58775487197ace7f92988f2f1ed7b623
jeffrevin <jeffrevin@gmail.com>
2020-07-07 14:08:24 -0400

    fixed startup scripts

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <header className="bg-blue-500">TEST HEADER</header>
+      <header className="bg-blue-500 text-center text-6xl">
+        Weather App
+      </header>
       <div className="bg-red-400">RED BG</div>
     </div>
   );
---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <form>
+      <section></section>
+    </form>
+  );
+};
+
+export default Form;
